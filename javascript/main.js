9e76d958e7e155ca0beeabebbd47123c37e43201
Eugene Balabat <eugene.balabat@gmail.com>
2020-10-09 23:32:26 +0300

    fixed repeating giphs, chanched lister from keyup to keypress(enter)

--- javascript/main.js
+++ javascript/main.js
@@ -2,38 +2,48 @@
 function pushToDOM(input) {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=0ZJRRFPcofu6YQDDqU6ghrleugGmYsKM&q=${input}&limit=25&offset=0&rating=g&lang=ru`;
   const conteiner = document.querySelector('.js-container');
-  const gifslink = new Array(0);
+  const gifslink = [];
 
   const GiphyAJAXCall = new XMLHttpRequest();
   GiphyAJAXCall.open('GET', url);
   GiphyAJAXCall.send();
 
   conteiner.innerHTML = '';
+  // console.clear();
 
   GiphyAJAXCall.addEventListener('load', (data) => {
     const res = JSON.parse(data.target.response);
 
     res.data.forEach((element) => {
-      const titleimage = element.title;
+      const giphid = element.id;
 
-      if (gifslink.indexOf(titleimage) === -1) {
-        const imageurl = element.images.original.webp;
+      if (!gifslink.includes(giphid)) {
         const contenblock = document.createElement('div');
+        const imageurl = element.images.original.webp;
+
+        // console.log(element);
 
         contenblock.innerHTML = `<a href = ${imageurl} target = "_blank"> <image src = ${imageurl} class = "image"> </a>`;
 
         conteiner.appendChild(contenblock).classList.add('container-image');
-        gifslink.push(titleimage);
-      } // else console.log('sd');
+        gifslink.push(giphid);
+      }
+      // gifslink.forEach((ele) => {
+      //    console.log(ele);
+      // });
     });
   });
 }
 
 /* 1. Grab the input value */
 
-document.querySelector('.js-userinput').addEventListener('keyup', () => {
-  pushToDOM(document.querySelector('input').value);
-});
+document
+  .querySelector('.js-userinput')
+  .addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      pushToDOM(document.querySelector('input').value);
+    }
+  });
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('.js-userinput').value = null;
